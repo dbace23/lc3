f523e0cb09645035989d034634c5ba919289a113
Halim Iskandar <40797564+dbace23@users.noreply.github.com>
2025-11-06 19:49:02 +0700

    Update like.go

--- model/like.go
+++ model/like.go
@@ -1,10 +1,11 @@
 package model
 
+import "time"
 type Like struct {
 	ID        int64  `json:"id"`
 	UserID    int64  `json:"user_id"`
 	PostID    int64  `json:"post_id"`
-	CreatedAt string `json:"created_at"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type CreateLikeReq struct {
