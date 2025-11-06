6fa06829ced532feb3cf3311342f57d0c8ac0878
Halim Iskandar <40797564+dbace23@users.noreply.github.com>
2025-11-06 14:28:13 +0700

    Update user.go

--- model/user.go
+++ model/user.go
@@ -1,13 +1,15 @@
 package model
 
+import "time"
+
 type User struct {
-	ID           int64  `json:"id"`
-	FirstName    string `json:"first_name"`
-	LastName     string `json:"last_name"`
-	Email        string `json:"email"`
-	Username     string `json:"username"`
-	PasswordHash string `json:"-"`
-	CreatedAt    string `json:"created_at"`
+	ID           int64     `json:"id"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	Email        string    `json:"email"`
+	Username     string    `json:"username"`
+	PasswordHash string    `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
 }
 
 // model/user.go
