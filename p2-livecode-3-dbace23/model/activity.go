fb74ab89f6de4f426d66b5b20f0df6ccd07b3065
Halim Iskandar <40797564+dbace23@users.noreply.github.com>
2025-11-06 12:58:53 +0700

    Delete p2-livecode-3-dbace23 directory

--- p2-livecode-3-dbace23/model/activity.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Activity struct {
-	ID          int64  `json:"id"`
-	UserID      int64  `json:"user_id"`
-	Action      string `json:"action"`
-	Description string `json:"description"`
-	CreatedAt   string `json:"created_at"`
-}
