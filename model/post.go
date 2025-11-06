f66570764eabbba34c3cfc2a92523cfe15b91910
Halim Iskandar <40797564+dbace23@users.noreply.github.com>
2025-11-06 19:27:30 +0700

    Update post.go

--- model/post.go
+++ model/post.go
@@ -1,11 +1,13 @@
 package model
 
+import "time"
+
 type Post struct {
-	ID        int64  `json:"id"`
-	Title     string `json:"title"`
-	Content   string `json:"content"`
-	AuthorID  int64  `json:"author_id"`
-	CreatedAt string `json:"created_at"`
+	ID        int64     `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	AuthorID  int64     `json:"author_id"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 // model/post.go
